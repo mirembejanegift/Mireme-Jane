e01d3c4ef27c324c4c0d9d80a25c5105d6b01e18
Mirembe Jane Mugeni <mirembejanegift@gmail.com>
2026-03-22 20:02:50 +0300

    my work of python

---
+++ ozzy.py
@@ -0,0 +1,20 @@
+# computer programming refers to the giving a directionn to a computer step by step so that it can perform tasks
+# Memory Partitioning refers to dividing a computer memory in to smaller parts, so different programs can use it at any time
+# (think of like a big room and you put in walls to make smaller room for different people to use) 
+# Memory address is the name given to the blocks in the computer memory after partitioning
+# (after creating the rooms those rooms are given names that we call memory address)
+# RAM connects to the CPU in a way that as RAM stores data and instruction, CPU processes them
+# (You are in the kitchen RAM will act as the table that holds ingredients while CPU will be you that is using the ingredients to cook)
+# A variable is like a box that stores information in acomputer for example all numbers and words are kept there
+# A statement is a single instruction that tells a computer what to do e.g this is just like a rule you can tell a robot to follow
+# A code is a a set of instructions that tells a computer what to do. This is just any  instruction you give a computer to tell it what to do.(this is like a sentence)
+# A block of code is a group of instructions that belong together these stay together and run instructinos(then this is a paragraphy)
+# syntax these are just rules of how code must be written so that computers understand it
+# (think of like how you speake English, for you to be able to speake and the people to understand, is the same as syntax in a computer)
+# examples include 
+# name = "jane"     here the variable which we called the box is (name) then jane is the (value)
+# age = "20"         same here age is the box and 20 the value
+# height = "5.0"   
+# my_num = 200
+# the computer is able to understand all because that is the languange of a computer
+# Declaring is the telling a computer that some thing exist and giving it a name
